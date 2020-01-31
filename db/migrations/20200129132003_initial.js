20b5105a1772a9f60c561ed3463cda9991395ce7
Alan Turing <austeyd@gmail.com>
2020-01-31 09:44:39 -0700

    Add table structure to migrations

--- db/migrations/20200129132003_initial.js
+++ db/migrations/20200129132003_initial.js
@@ -1,15 +1,27 @@
-
 exports.up = function(knex) {
   return knex.schema
-  .createTable('papers', function (table) {
+  .createTable('teams', function (table) {
     table.increments('id').primary();
-    table.string('title');
-    table.string('author');
-
+    table.string('team');
+    table.string('city');
+    table.string('total championships');
     table.timestamps(true, true);
+  })
+  .createTable('champions', table => {
+    table.increments('id').primary();
+    table.string('champion');
+    table.string('year');
+    table.string('opponent');
+    table.integer('team_id').unsigned();
+    //Unsigned means no (-) attached, no negative numbers
+    table.foreign('team_id').references('team.id');
+    //Makes this a foreign key referencing the primary key on the other table
   })
 };
 
 exports.down = function(knex) {
-
+  //In removing the tables, the attached table is first removed, then the anchor
+  return knex.schema
+    .dropTable('champions')
+    .dropTable('teams')
 };

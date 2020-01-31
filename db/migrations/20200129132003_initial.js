e41e667987e425a81c0b6b0e462907bee341129d
Alan Turing <austeyd@gmail.com>
2020-01-31 09:24:31 -0700

    Add Procfile

--- db/migrations/20200129132003_initial.js
+++ db/migrations/20200129132003_initial.js
@@ -1,8 +1,15 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema
+  .createTable('papers', function (table) {
+    table.increments('id').primary();
+    table.string('title');
+    table.string('author');
+
+    table.timestamps(true, true);
+  })
 };
 
 exports.down = function(knex) {
-  
+
 };

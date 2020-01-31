f0c16e4828272885ebd1e0edd2c13f992f9cdfdb
Alan Turing <austeyd@gmail.com>
2020-01-31 09:51:37 -0700

    Establish seed files

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 
 //Example code from Express lesson
-// app.set('port', process.env.PORT || 3000);
-// app.locals.title = 'Pet Box';
-//
+app.set('port', process.env.PORT || 3000);
+app.locals.title = 'BYOB_Nba';
+
 // app.get('/', (request, response) => {
 //   response.send('Oh hey Pet Box');
 // });
 //
-// app.listen(app.get('port'), () => {
-//   console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
-// });
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
+});

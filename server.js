1557f3dfbd25cf4f40ce996e43e2ecd07edc6c84
Alan Turing <austeyd@gmail.com>
2020-01-31 09:46:21 -0700

    Add placeholder code to server file

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+//Example code from Express lesson
+// app.set('port', process.env.PORT || 3000);
+// app.locals.title = 'Pet Box';
+//
+// app.get('/', (request, response) => {
+//   response.send('Oh hey Pet Box');
+// });
+//
+// app.listen(app.get('port'), () => {
+//   console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
+// });

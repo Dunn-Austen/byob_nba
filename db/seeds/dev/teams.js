e6291d5330697960048db4091fddb4b40e1761d2
Alan Turing <austeyd@gmail.com>
2020-02-01 15:20:45 -0700

    Update teams.js with map functionality

--- db/seeds/dev/teams.js
+++ db/seeds/dev/teams.js
@@ -1,13 +1,41 @@
+const teamsData = require('../../../teamsData');
 
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
+const createTeam = async (knex, team) => {
+  const teamId = await knex('teams').insert({
+    franchise: team.franchise,
+    playoff_series: team.Plyfs,
+    champsionships: team.champ
+  }, 'id');
+
+  let championPromises = team.champions.map(champion => {
+    return createChampion(knex, {
+      champ: champion,
+      team_id: teamId[0]
+    })
+  });
+
+  return Promise.all(championPromises);
+};
+
+const createChampion = (knex, champion) => {
+  return knex('champions').insert(champion);
+};
+
+exports.seed = async function(knex) {
+  try {
+    await knex('teams').del();
+    await knex('champions').del();
+    //Deletes the data in the table
+
+    let teamPromises = teamsData.map(team => {
+      return createTeam(knex, team);
     });
+
+    return Promise.all(teamPromises);
+
+    //I need to clarify 'knex' - return (knex)
+
+  } catch (error) {
+    console.log(`Error sending data: ${error}`);
+  }
 };

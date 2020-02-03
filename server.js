5289149c27a360c17c9c152dcc9668543433051e
Alan Turing <austeyd@gmail.com>
2020-02-02 18:18:07 -0700

    Build get endpoints for champions

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const database = require('knex')(configuration);
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'BYOB_NBA';
+//app.use(exp)
 
 app.get('/', (request, response) => {
   response.send('Welcome to the BYOB_NBA page');
@@ -21,17 +22,41 @@ app.get('/api/v1/teams', async (request, response) => {
   }
 });
 
-//Get request for a specific team - needs amendment for final lesson
+//Get request for a specific team
 app.get('/api/v1/teams/:id', async (request, response) => {
   try {
     const { id } = request.params;
     const teams = await database('teams').select();
-    const team = teams.find(team => team.id === id);
+    const team = teams.find(team => team.id === Number(id));
     if (!team) {
-      return response.sendStatus(404);
+        return response.sendStatus(404);
     }
-
     response.status(200).json({team});
+  } catch (error) {
+    response.status(500).json({error})
+  }
+});
+
+//Get request for all champions
+app.get('/api/v1/champions', async (request, response) => {
+  try {
+    const champions = await database('champions').select();
+    response.status(200).json({champions});
+  } catch (error) {
+    response.status(500).json({error})
+  }
+});
+
+//Get request for a specific champion
+app.get('/api/v1/champions/:id', async (request, response) => {
+  try {
+    const { id } = request.params;
+    const champions = await database('champions').select();
+    const champion = champions.find(champion => champion.id === Number(id));
+    if (!team) {
+        return response.sendStatus(404);
+    }
+    response.status(200).json({champion});
   } catch (error) {
     response.status(500).json({error})
   }

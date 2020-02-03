0da50f44745ac7734734acb26a6ccec7451733cf
Alan Turing <austeyd@gmail.com>
2020-02-03 13:54:48 -0700

    Add annotations to GET all teams block

--- server.js
+++ server.js
@@ -14,31 +14,44 @@ app.use(express.json());
 
 //Dictates the port on which the server runs (specifying 3000 as the default)
 app.set('port', process.env.PORT || 3000);
-//The locals object is somewhat akin to local storage - a sometimes convenient means for caching information. Here we're just storing the title
+//The locals object is somewhat akin to local storage - a sometimes convenient means for caching information. Here we're just storing the title.
+//App.locals is provided by express
 app.locals.title = 'BYOB_NBA';
 
 
 app.get('/', (request, response) => {
-//This express route refers to the instace of our express app handling a get request (method) to the '/'  (the first argument). The 2nd argument is a callback that handles the logic governing the reaction to the get request
+//This express route refers to the instace of our express app handling a get request (method) to the '/' (path) (the first argument). The 2nd argument is a callback that handles the logic governing the reaction to the get request
   response.send('Welcome to the BYOB_NBA page');
 //Line 23 returns a reponse with the specified text
 });
 
-
+//This route involves the logic for the type of response provided for a Get request for all teams
+//Same instance(app)/method(get)/path(/api/v1/teams)/handler format as the above endpoint
 app.get('/api/v1/teams', async (request, response) => {
-  try {
+  //The Try / Catch statements allow for an error to be thrown if the try statement fails to execute appropriately, like an if/else statement
+  try
+  //Assuming no error, the following code dictates the promise resolution
+  //Initializes the variable, teams, with data from the teams table (the select call resolves to an array of objects from the database)
     const teams = await database('teams').select();
+  //
     response.status(200).json({teams});
+    //The 200 status indicates a successful get request, and gives a response object that amounts to the parsed data from the teams database
   } catch (error) {
+    //Catch block runs the below code if an error is caught in the try statement
+    //Gives back a 500 status, with parsed text explaining that a 500 code corresponds to an internal server error
     response.status(500).json({error: 'internal server error' })
   }
 });
-//This is the GET request enpoint for all team data. The code block suffices to specify what to do when encountering a particular fetch call (determines happy & sad paths and targeted responses)
-//Each of the remaining code blocks represents the programmed logic for receiving and processing various fetch calls, be they successful or some other condition
 
+//This route governs the behavior responding to a GET request for an individual team's data
+//Same instance(app)/method(get)/path(/api/v1/teams/id:)/handler format as the above endpoint
+//The :id refers to an identifier included in the table creation for the teams data, and allows us to pinpoint particular objects from the data set
 app.get('/api/v1/teams/:id', async (request, response) => {
   try {
+    //Includes the promise resolution in the case of a successful fetch request
     const teams = await database('teams').where('id', request.params.id).select();
+    //This code is largely the same as above, with the exception of the WHERE statement
+    //
     if (team.length) {
       response.status(200).json({team});
 
